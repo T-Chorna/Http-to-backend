fcd25aaff06b43469205102567245f7a5f8b48b4
T-Chorna <groupdg617@gmail.com>
2024-08-20 16:17:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,21 +6,29 @@ async function DataTable(config) {
     return;
   }
   parentElement.innerHTML = "";
+  parentElement.style.display = 'flex';
+  parentElement.style.flexDirection = 'column';
+
+  let btnAddData = document.createElement("button");
+  btnAddData.setAttribute("class", "btn-add-data");
+  btnAddData.innerHTML = "Додати";
+  parentElement.appendChild(btnAddData);
 
   let table = document.createElement('table');
   table.appendChild(createHeadRow(config));
   table.appendChild(await createBodyRow(config));
+  table.appendChild(createModalInput(config));
   parentElement.appendChild(table);
 }
 
 function createHeadRow(config){
   let head = document.createElement('thead');
   let titleRow = document.createElement('tr');
-  titleRow.appendChild(createCell('th', '№'));
+  titleRow.appendChild(createElement('th', '№'));
   for(let i = 0; i < config.columns.length; i++){
-    titleRow.appendChild(createCell('th', config.columns[i].title));
+    titleRow.appendChild(createElement('th', config.columns[i].title));
   }
-  titleRow.appendChild(createCell('th', 'Дії'));
+  titleRow.appendChild(createElement('th', 'Дії'));
   return head.appendChild(titleRow);
 }
 
@@ -34,18 +42,19 @@ async function createBodyRow(config){
     let row = document.createElement('tr');
     for(let i = 0; i <= config.columns.length; i++){
       if(i === 0){
-        row.appendChild(createCell('td', ++counterRow));
+        row.appendChild(createElement('td', ++counterRow));
         continue;
       }
       let column = config.columns[i-1];
       if(typeof config.columns[i-1].value === 'string'){
-        row.appendChild(createCell('td', value[column.value]));
+        row.appendChild(createElement('td', value[column.value]));
       } else{
-        row.appendChild(createCell('td', column.value(value)))
+        row.appendChild(createElement('td', column.value(value)))
       }
     }
-    let actionCell = createCell('td',"");
-    let button = createCell('button', "Видалити");
+    let actionCell = createElement('td',"");
+    let button = createElement('button', "Видалити");
+    button.setAttribute('class', 'btn-delete');
     button.onclick = () => {deleteItem(key, config)};
     actionCell.appendChild(button);
     row.appendChild(actionCell);
@@ -53,6 +62,33 @@ async function createBodyRow(config){
   }
   return body;
 }
+
+function createModalInput(config){
+  let modalOverlay = createElement("div", "");
+  modalOverlay.setAttribute("class", "modal-overlay");
+  let modal = createElement("div","<h2>Modal</h2>");
+  modal.setAttribute("class", "modal");
+  let form = createElement('form',"");
+
+  for(let i = 0; i < config.columns.length; i++){
+    let input = config.columns[i].input;
+    if(!Array.isArray(input)){
+      let label = input.label?input.label:config.columns[i];
+      let labelElement = createElement("label", label);
+    }
+  }
+
+  modalOverlay.appendChild(modal);
+  return modalOverlay;
+}
+
+{/* <div class="modal-overlay" id="modalOverlay">
+<div class="modal">
+    <h2>Модальне вікно</h2>
+    <p>Це просте і симпатичне модальне вікно. Ви можете додати сюди будь-який вміст.</p>
+    <button onclick="closeModal()">Закрити</button>
+</div>
+</div> */}
 
 async function sendRequest(url, method){
   try {
@@ -74,10 +110,10 @@ async function deleteItem(itemId, config){
   await DataTable(config)
 }
 
-function createCell(typeCell, content){
-  let cell = document.createElement(typeCell);
-  cell.innerHTML = content;
-  return cell;
+function createElement(typeElement, content){
+  let elem = document.createElement(typeElement);
+  elem.innerHTML = content;
+  return elem;
 }
 
 function getAge(birthDateString){
@@ -101,6 +137,11 @@ function getAge(birthDateString){
   return `${yearDiff} year ${monthDiff} month ${dayDiff} day`
 }
 
+function getColorLabel(color){
+  let square = `<div style="width:100px; height:100px; background-color:${color}"></div>"`;
+  return square;
+}
+
 const config1 = {
  parent: '#usersTable',
  columns: [
@@ -112,4 +153,31 @@ const config1 = {
  apiUrl: "https://mock-api.shpp.me/tchorna/users"
 };
 
-DataTable(config1);
+// DataTable(config1);
+
+const config2 = {
+  parent: '#productsTable',
+  columns: [
+    {
+      title: 'Назва', 
+      value: 'title', 
+      input: { type: 'text' }
+    },
+    {
+      title: 'Ціна', 
+      value: (product) => `${product.price} ${product.currency}`,
+      input: [
+        { type: 'number', name: 'price', label: 'Ціна' },
+        { type: 'select', name: 'currency', label: 'Валюта', options: ['$', '€', '₴'], required: false }
+      ]
+    },
+    {
+      title: 'Колір', 
+      value: (product) => getColorLabel(product.color), // функцію getColorLabel вам потрібно створити
+      input: { type: 'color', name: 'color' }
+    }, 
+  ],
+  apiUrl: "https://mock-api.shpp.me/tchorna/products"
+};
+
+DataTable(config2);
